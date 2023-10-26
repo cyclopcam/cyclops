a7c990ea23204a4575c8ac8748392ee03bb7d12e
Ben Harper <rogojin@gmail.com>
2023-10-26 07:43:46 +0200

    Bump keyframe interval of arc videos from 10 to 8

--- pkg/videox/transcode.go
+++ pkg/videox/transcode.go
@@ -20,7 +20,7 @@ func TranscodeSeekable(srcFilename, dstFilename string) error {
 }
 
 // Transcode the high quality video stream into a slightly lower quality stream,
-// with keyframes every 10 frames, and with noise reduction. This is for use on
+// with keyframes every 8 frames, and with noise reduction. This is for use on
 // our training platform, where people need to be able to seek randomly inside
 // a video.
 func TranscodeMediumQualitySeekable(srcFilename, dstFilename string) error {
@@ -31,7 +31,7 @@ func TranscodeMediumQualitySeekable(srcFilename, dstFilename string) error {
 		"removegrain=1,hqdn3d=1.5:1.5:10:6,scale=1400:-1",
 		"-y",   // overwrite output file
 		"-g",   // keyframe interval
-		"10",   // keyframe every 3 frames
+		"8",    // keyframe every X frames
 		"-crf", // constant rate factor
 		"25",   // 0-51, 0 is lossless, 51 is worst quality
 		dstFilename,
